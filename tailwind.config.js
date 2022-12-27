997e263c1c2b36a7b90dd9903f833643227574d4
NPotokin <npotokin@gmail.com>
2022-12-27 11:20:19 -0700

    initial set up

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    //colors: {
+      //'main1': '#000000',
+      //'main2': '#ffffff',
+      //'backgr1': '#ffffff',
+      //'backgr2': '#ffffff',
+      //'accent1': '#ffffff',
+      //'accent2': '#ffffff',
+      //},
+      //fontFamily: {
+        //sans: ['Montserrat', 'sans-serif'],
+        //serif: ['Merriweather', 'serif'],
+     // },
+    extend: {},
+  },
+  plugins: [],
+}
